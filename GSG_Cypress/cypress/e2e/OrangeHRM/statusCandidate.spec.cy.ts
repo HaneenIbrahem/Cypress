4bccbc44a649f2c7cc71f3e4e29e691fff4d4878
HaneenIbrahem <haneen.m.ibrahem@gmail.com>
2023-10-14 22:47:09 +0300

    task2

---
+++ GSG_Cypress/cypress/e2e/OrangeHRM/statusCandidate.spec.cy.ts
@@ -0,0 +1,25 @@
+import CandidatesCount from "../../support/pageObjects/candidates"
+import LoginValidation from "../../support/pageObjects/loginValidation"
+
+const count : CandidatesCount = new CandidatesCount();
+const loginObjValidation : LoginValidation = new LoginValidation();
+
+describe("Login Home Page", () => {
+    beforeEach(function (){
+        cy.visit("/web/index.php/auth/login");
+        cy.fixture('login').as('data')
+
+        cy.get('@data').then((infoData:any)=>{
+            loginObjValidation.fillData(infoData.valid.name,infoData.valid.password)
+            loginObjValidation.checkPage(infoData.valid.message)
+        })
+    })
+
+    it("candidate count",()=>{
+        count.addNewCandidateViaAPI("no comment", false, '0123456789', "2023-10-14", "hh@hh.hh", "haneen", "hi", "Ibrahem", "MMM", 8)
+        count.shortlistCandidate("init note")
+        count.scheduleInterview()
+        
+    })
+
+})
